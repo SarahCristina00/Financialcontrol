7db53a40102083d9ea18060a73146f5d37382bc3
sarah cristina <sarahsilva34567@gmail.com>
2025-03-25 22:46:39 -0300

    Cria projeto Spring Boot Financialcontrol

---
+++ src/test/java/com/Financialcontrol/Financialcontrol/FinancialcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Financialcontrol.Financialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
